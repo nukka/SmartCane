9c6fa17f178b87cd04c19025d47e25a0c04fef80
Annukka Törmälä <tormala.annukka@gmail.com>
2017-11-20 18:18:03 +0200

    add main menu

---
+++ Assets/MainMenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuButtons : MonoBehaviour {
+
+
+	public void StartBtn (string NewSimulationLevel) {
+		SceneManager.LoadScene (NewSimulationLevel);
+		
+	}
+
+	public void quitBtn() {
+		Application.Quit ();
+	}
+
+	
+
+}
