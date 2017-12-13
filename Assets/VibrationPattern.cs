8bc5e32a8fd347ffc278199e20b6ccd5e24dab5e
Annukka <tormala.annukka@gmail.com>
2017-12-13 20:32:04 +0200

    add vibration through Rewired

---
+++ Assets/VibrationPattern.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using Rewired;
+
+[RequireComponent(typeof(CharacterController))]
+public class VibrationPattern : MonoBehaviour {
+
+	public int playerId = 0; // The Rewired player id of this character
+
+	public float moveSpeed = 3.0f;
+	public float bulletSpeed = 15.0f;
+	public GameObject bulletPrefab;
+
+	private Player player; // The Rewired Player
+	private CharacterController cc;
+	private Vector3 moveVector;
+
+
+	void Awake() {
+		// Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
+		player = ReInput.players.GetPlayer(playerId);
+
+
+	}
+
+	public void Vibrate(int motorIndex, float motorLevel, float duration){
+		player.SetVibration(motorIndex, motorLevel, duration);
+
+	}
+}
--- Assets/VibrationScript.cs
+++ Assets/VibrationScript.cs
@@ -5,22 +5,26 @@ using XInputDotNetPure;
 using Rewired;
 
 public class VibrationScript : MonoBehaviour {
+
+	public VibrationPattern _VibrationPattern;
+
 	void OnCollisionEnter (Collision collision) {
 		if (collision.collider.tag == "RegularPoint") {
-			GamePad.SetVibration (0, 0.2f, 0.2f);
+			//GamePad.SetVibration (0, 0.2f, 0.2f);
+			_VibrationPattern.Vibrate(0, 0.5f, 0.5f);
 
 		}	
 		if (collision.collider.tag == "POI") {
-			GamePad.SetVibration (0, 1f, 1f);
+			_VibrationPattern.Vibrate(0, 1f, 0.5f);
 		}	
 	}
 
 	void OnCollisionStay (Collision collision) {
 		if (collision.collider.tag == "RegularPoint") {
-			GamePad.SetVibration (0, 0.2f, 0.2f);
+			_VibrationPattern.Vibrate(0, 0.5f, 0.5f);
 		}	
 		if (collision.collider.tag == "POI") {
-			GamePad.SetVibration (0, 1f, 1f);
+			_VibrationPattern.Vibrate(0, 1f, 0.5f);
 		}	
 	}
 
